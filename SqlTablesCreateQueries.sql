63ef29b42b833e167005f0eb8d35140de61c6ebb
seyda zenler <seydaazenler@gmail.com>
2021-12-06 13:42:38 +0300

    Kitaplar INSERT yapıldı

---
+++ SqlTablesCreateQueries.sql
@@ -0,0 +1,34 @@
+﻿--Öğrenciler tablosu eklendi
+
+--Yazarlar tablosu eklendi
+
+--Kitaplar tablosu eklendi
+
+--Turler tablosu eklendi
+
+--İşlem tablosu eklendi
+create table Islem
+(
+IslemID int identity(1,1) Primary Key,
+OgrID int not null,
+KitapID int not null,
+OduncAldigiTarih datetime not null,
+OduncBitisTarih datetime not null
+)
+
+--Kitaplar INSERT yapıldı
+INSERT INTO [dbo].[Kitaplar]
+           ([KayitTarihi]
+           ,[KitapAdi]
+           ,[SayfaSayisi]
+           ,[SilindiMi]
+           ,[TurID]
+           ,[YazarID])
+     VALUES
+           (GETDATE(),
+		   'Serenad',
+		   150,
+		   0,
+		   1,
+		   1
+		   )
